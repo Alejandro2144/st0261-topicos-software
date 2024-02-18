c6338cff5ebf60e316a4b1231655aac7553194a9
Alejandro Torres <edialeto@hotmail.com>
2024-02-17 22:15:11 -0500

    Laravel practica 1 added

---
+++ practica-laravel-1/laravelcourse/resources/views/product/created.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Product created')
+@section('content')
+    <div class="alert alert-success" role="alert">
+        Product created
+    </div>
+@endsection
