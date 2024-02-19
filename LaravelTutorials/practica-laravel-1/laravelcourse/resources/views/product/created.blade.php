602b0ac0bdba931a2cec701d1f7d0983221459da
Alejandro Torres <edialeto@hotmail.com>
2024-02-19 08:14:28 -0500

    Laravel tutorial 1 added

--- LaravelTutorials/practica-laravel-1/laravelcourse/resources/views/product/created.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Product created')
-@section('content')
-    <div class="alert alert-success" role="alert">
-        Product created
-    </div>
-@endsection
