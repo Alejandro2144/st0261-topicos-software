58362013c3a328f21b23bdc8f3737f4f48306993
Alejandro Torres <edialeto@hotmail.com>
2024-04-01 08:33:34 -0500

    Tutorial 4 added

---
+++ LaravelTutorials/LaravelTutorial4/laravelcourse/app/Http/Controllers/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
+    }
+
+    public function show(string $id): View|RedirectResponse
+    {
+        $viewData = [];
+        $product = Product::find($id);
+
+        if (! $product) {
+            return redirect()->route('home.contact');
+        }
+
+        $viewData['title'] = $product->name.' - Online Store';
+        $viewData['subtitle'] = $product->name.' - Product information';
+        $viewData['price'] = $product->price.' - Product price';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
+    }
+
+    public function create(): View
+    {
+        $viewData = []; //to be sent to the view
+        $viewData['title'] = 'Create product';
+
+        return view('product.create')->with('viewData', $viewData);
+    }
+
+    public function save(Request $request): View|RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric|min:1',
+        ]);
+
+        Product::create($request->only(['name', 'price']));
+
+        return view('product.created');
+    }
+}
